a5aa268e2cb8b561a4c41da217cfc4b6a4a3c6a1
shahlarizki22 <sahlarizki5@gmail.com>
2025-02-12 10:39:55 +0700

    Nih Bos

--- Login/index.php
+++ Login/index.php
@@ -30,7 +30,7 @@ if ($_SERVER["REQUEST_METHOD"] == "POST" && isset($_POST["login"])) {
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Login</title>
+    <title>Login Nih</title>
     <link rel="stylesheet" href="stylesal.css">
 </head>
 <body>
